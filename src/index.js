d268635044c6b12abedaf55d791797e636e2f8ab
JaderSabino <jader.sabino2014@gmail.com>
2020-02-10 17:59:39 -0300

    Primeiro commit - API Node

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const body_parser = require('body-parser');
+
+const app = express();
+
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: false }));
+
+app.listen(3000, () => {
+    console.log('Server em execução');
+    console.log('pressione CTRL+C para derrubar');
+});
+
+app.get('/teste', (req, res) => {
+    res.send('teste nodemon 2');
+});
