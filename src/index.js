56361611bbfce1eebc2855e409ae900d1ac6f2e6
JaderSabino <jader.sabino2014@gmail.com>
2020-02-11 00:45:31 -0300

    Conecção Mongo DB. Criação de usuarios. Encryptação de senhas com bcryptjs. Autenticação de usuarios

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ const app = express();
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
 
+require('./controllers/authController')(app);
+
 app.listen(3000, () => {
     console.log('Server em execução');
     console.log('pressione CTRL+C para derrubar');
-});
-
-app.get('/teste', (req, res) => {
-    res.send('teste nodemon 2');
 });
