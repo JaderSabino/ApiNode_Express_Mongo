436309beb9d394da9f34f46fb77fe5ec29d08d45
JaderSabino <jader.sabino2014@gmail.com>
2020-02-12 00:27:38 -0300

    autenticação de usuarios, geração de tokens e valdiação com middlewares

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
 
 require('./controllers/authController')(app);
+require('./controllers/projectController')(app);
 
 app.listen(3000, () => {
     console.log('Server em execução');
